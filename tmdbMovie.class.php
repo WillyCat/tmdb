5d02efc85efec565344dd832fad8e26d1e2d0045
François <fh@iMac-de-Francois.local>
2022-08-27 15:25:26 +0200

    20220827

--- tmdbMovie.class.php
+++ tmdbMovie.class.php
@@ -1,9 +1,9 @@
 <?php
 class tmdbMovie extends tmdbRequest {
         public function
-        __construct(int $movie_id, string $language = 'fr-FR', ?callable $proxy = null)
+        __construct(int $movie_id, string $language = 'fr-FR', ?callable $proxy = null, $keyfile = '')
         {
-                parent::__construct (path: 'movie/'.$movie_id, parms: [ 'language' => $language ], proxy: $proxy);
+                parent::__construct (path: 'movie/'.$movie_id, parms: [ 'language' => $language ], proxy: $proxy, keyfile: $keyfile);
         }
 }
 ?>
